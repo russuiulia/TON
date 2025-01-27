432dd7ef34f73863d025a5331806de7f9504104d
russuiulia <russuiulia.md@gmail.com>
2025-01-27 06:42:38 +0200

    aaa

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -59,7 +59,7 @@ export const App: FC = () => {
           {routes.map((route) => (
             <Route key={route.path} {...route} />
           ))}
-          <Route path="*" element={<Navigate to="/" />} />
+          {/* <Route path="*" element={<Navigate to="/" />} /> */}
         </Routes>
       </Router>
     </AppRoot>
